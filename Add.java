ee94fabe41ac732186fa0421cc5871567b50202a
sandeep <sandeepkaey@gmail.com>
2023-03-24 13:14:03 +0530

    first initial commit

---
+++ Add.java
@@ -0,0 +1,14 @@
+class Add
+{
+   static void add()
+   {
+     int a=10;
+     int b=20;
+     int c=a+b;
+     System.out.println("the result of a & b is "+c);
+     }
+     public static void main(String[]args)
+     {
+       add();
+       }
+       }
